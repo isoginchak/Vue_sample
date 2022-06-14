d300235f04e5974b07d7e200020144a31674793b
Minatsu Iso <isoginchak0825@gmail.com>
2022-06-14 17:12:52 +0900

    feat:add api

--- sample03/axios_app/src/components/HelloWorld.vue
+++ sample03/axios_app/src/components/HelloWorld.vue
@@ -2,7 +2,8 @@
   <section class="alert alert-primary">
     <h1>{{ data.title }}</h1>
     <p>{{ data.message }}</p>
-    <textarea v-model="data.mydata" rows="5" class="form-control"></textarea>
+     <p>{{data.mydata }}</p>
+    <textarea v-model="data.info" row="5" class="form-control"></textarea>
   </section>
 </template>
 
@@ -10,18 +11,23 @@
 import axios from "axios";
 import { reactive, onMounted } from "vue";
 
-const url = "/data.txt";
+const url = "https://randomuser.me/api/";
 
 export default {
   setup(props) {
     const data = reactive({
       title: "Axios",
       message: "This  is axios sample.",
+      info: "",
       mydata: "",
+      
     });
     const getData = async () => {
       let result = await axios.get(url);
-      data.mydata = result.data;
+      console.log(result);
+
+      data.info = JSON.stringify(result.data.info.page);
+      data.mydata = JSON.stringify(result.data);
     };
     onMounted(() => {
       getData();
